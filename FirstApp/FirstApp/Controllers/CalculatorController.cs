1ca1a86111086f4e05e081e7e3ae406daf43e0b2
gilbertotakeya <gilbertotakeya@yahoo.com.br>
2020-03-10 19:50:21 -0300

    FirstAPP

---
+++ FirstApp/FirstApp/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstApp.Models;
+using System.IO;
+
+namespace FirstApp.Controllers
+{
+    [Route("Calculadora")]
+    public class CalculatorController : Controller
+    {
+        public CalculatorController()
+        {            
+        }
+
+        [Route("Soma/{valor1:int}/{valor2:int}")]
+        public IActionResult Plus(int valor1, int valor2)
+        {
+            return Content(Convert.ToString(valor1 + valor2));
+        }
+
+        [Route("Menos/{valor1:int}/{valor2:int}")]
+        public IActionResult Minus(int valor1, int valor2)
+        {
+            return Content(Convert.ToString(valor1 - valor2));
+        }
+
+        [Route("Multiplicacao/{valor1:int}/{valor2:int}")]
+        public IActionResult Multiplication(int valor1, int valor2)
+        {
+            if (valor1 == 0 || valor2 == 0)
+                return BadRequest("Valor não pode ser zero!");
+
+            return Content(Convert.ToString(valor1 * valor2));
+        }
+
+        [Route("Divisao/{valor1:int}/{valor2:int}")]
+        public IActionResult Division(int valor1, int valor2)
+        {
+            if (valor1 == 0 || valor2 == 0)
+                return BadRequest("Valor não pode ser zero!");
+
+            return Content(Convert.ToString(valor1/valor2));
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstApp.Models;
+using System.IO;
+
+namespace FirstApp.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("pagina-inicial")]
+        public IActionResult Index(int id)
+        {                        
+            var fileBytes = System.IO.File.ReadAllBytes(@"C:\arquivo.txt");
+            var fileName = "teste_maravilhoso.txt";
+
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+            //return Content("Esse é um retorno legal");
+            //return Ok();
+            return BadRequest();
+        }
+
+        [Route("privacidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
